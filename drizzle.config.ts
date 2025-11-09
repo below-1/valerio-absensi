4c8e3eed409a26052879e7a3a7363b2852eaca83
below-1 <jordanmeta4677@gmail.com>
2025-11-10 01:09:09 +0800

    add turso dialect

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,9 +3,10 @@ import { defineConfig } from "drizzle-kit";
 export default defineConfig({
   schema: "./src/lib/db/schema.ts",
   out: "./drizzle",
-  dialect: "sqlite",
+  dialect: "turso",
   dbCredentials: {
     url: process.env.DATABASE_URL ?? "file:local.db",
+    authToken: process.env.DATABASE_AUTH_TOKEN ?? "",
   },
   verbose: true,
 	strict: true
